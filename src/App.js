8e7d0bc798eea49468c8118ef73105ee9b82f61f
Sam Nick <omprakash121samuni@gmail.com>
2024-02-11 21:55:45 +0530

    nt

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Sam , How are You!</p>
+        <a
+          className="App-link"
+          href="https://samnickgammer.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SamNickGammer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
